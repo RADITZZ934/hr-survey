ee86e1844b0a2c804a83220be6ebe10a4f90781c
raditya <radityadamargalih12@gmail.com>
2026-07-24 15:58:02 -0700

    3

--- backend/config/app.go
+++ backend/config/app.go
@@ -17,17 +17,26 @@ type Config struct {
 
 var AppConfig Config
 
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
 func LoadConfig() {
 	AppConfig = Config{
-		Port: os.Getenv("1302"),
+		Port: getEnv("PORT", "1302"),
 
-		DBHost:     os.Getenv("localhost"),
-		DBPort:     os.Getenv("5432"),
-		DBUser:     os.Getenv("postgres"),
-		DBPassword: os.Getenv("postgres"),
-		DBName:     os.Getenv("hr_survey"),
-		DBSSLMode:  os.Getenv("disable"),
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "hr_survey"),
+		DBSSLMode:  getEnv("DB_SSLMODE", "disable"),
 
-		JWTSecret: os.Getenv("supersecret123"),
+		JWTSecret: getEnv("JWT_SECRET", "supersecret123"),
 	}
 }
+
